685ab0d0317074ab769aa4abd1053c97a70eb032
NoizetRomain <romain.noizet@epitech.eu>
2022-10-25 10:45:55 +0200

    deleting api directory

--- api/priv/repo/migrations/20221024135433_workingtime_belongs_to_user.exs
+++
@@ -1,9 +0,0 @@
-defmodule Api.Repo.Migrations.WorkingtimeBelongsToUser do
-  use Ecto.Migration
-
-  def change do
-    alter table(:workingtimes) do
-      add :user, references(:users)
-    end
-  end
-end
--- api/priv/repo/migrations/20221024142911_clock_belongs_to_user.exs
+++
@@ -1,9 +0,0 @@
-defmodule Api.Repo.Migrations.ClockBelongsToUser do
-  use Ecto.Migration
-
-  def change do
-    alter table(:clocks) do
-      add :user, references(:users)
-    end
-  end
-end
