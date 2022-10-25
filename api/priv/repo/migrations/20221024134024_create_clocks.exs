65c044f9767deaa7bae242d953cca249220448a4
NoizetRomain <romain.noizet@epitech.eu>
2022-10-25 09:51:56 +0200

    pushing api

---
+++ api/priv/repo/migrations/20221024134024_create_clocks.exs
@@ -0,0 +1,12 @@
+defmodule Api.Repo.Migrations.CreateClocks do
+  use Ecto.Migration
+
+  def change do
+    create table(:clocks) do
+      add :time, :naive_datetime
+      add :status, :boolean, default: false, null: false
+
+      timestamps()
+    end
+  end
+end
