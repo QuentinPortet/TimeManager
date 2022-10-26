9f1a92398fb95270866d437c21b055498e1ca4c3
paralethadox <keenlefou@hotmail.com>
2022-10-26 14:20:57 +0200

    [*] Changed project architecture

---
+++ timeManagerClient/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import UserComponent from "./components/UserComponent.vue";
+</script>
+
+<template>
+  <header>
+    <div style="border-ra">
+      <UserComponent></UserComponent>
+    </div>
+  </header>
+</template>
+
+<style scoped></style>
---
+++ timeManagerClient/src/components/UserComponent.vue
@@ -0,0 +1,33 @@
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      username: "rien",
+      email: "rien",
+      info: null,
+    };
+  },
+  mounted() {
+    axios
+      .get("http://localhost:4000/api/users/1", {
+        headers: { "Access-Control-Allow-Origin": "*" },
+      })
+      .then((response) => (this.info = response));
+    console.log("reponse: ", this.info);
+  },
+};
+</script>
+
+<template>
+  <div class="card">
+    <div class="fancy-stripe">-</div>
+    <div class="header">
+      {{ username }}
+    </div>
+    <div class="content">
+      {{ email }}
+    </div>
+  </div>
+</template>
