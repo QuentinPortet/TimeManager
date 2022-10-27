b0f597dfc362a610ae6106da41499d1d52c74eb9
NoizetRomain <romain.noizet@epitech.eu>
2022-10-27 12:04:01 +0200

    API - pause clope

--- api/lib/api_web/controllers/clock_controller.ex
+++ api/lib/api_web/controllers/clock_controller.ex
@@ -14,6 +14,10 @@ defmodule ApiWeb.ClockController do
 
   def create(conn, %{"time" => time, "status" => status, "userID" => user_id}) do
     user = String.to_integer(user_id)
+    clock = Accounts.get_latest_clock_by_user(user_id)
+
+    Logger.info(clock)
+
     with {:ok, %Clock{} = clock} <- Accounts.create_clock(%{time: time, status: status, user_id: user}) do
       conn
       |> put_status(:created)
