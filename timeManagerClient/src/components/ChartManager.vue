9266a7eaa61024932ef7bc57ec5ee6254e9df23b
Arthur <arthur.rochette@gmail.com>
2022-11-02 16:30:53 +0100

    [+] bar charts

--- timeManagerClient/src/components/ChartManager.vue
+++ timeManagerClient/src/components/ChartManager.vue
@@ -32,7 +32,15 @@ export default {
       userClocks: [],
       diffLastClock: 0,
       barsData: {
-        labels: ["January", "February", "March"],
+        labels:  [
+          "Monday",
+          "Tuesday",
+          "Wednesday",
+          "Thursday",
+          "Friday",
+          "Saturday",
+          "Sunday",
+        ],
         datasets: [
           {
             label: "Quelque chose",
@@ -125,15 +133,18 @@ export default {
     },
     async getWorkingTimeEver() {
       let date = new Date(Date.now());
-      let dateMinus7 = new Date(Date.now(0));
+      let epoch0 = new Date(0);
+      console.log(epoch0);
+      console.log(date);
       try {
         let res = await axios
           .get("http://localhost:4000/api/workingtimes/" + this.userid, {
-            params: { start: dateMinus7, end: date },
+            params: { start: epoch0, end: date },
             header: "Access-Control-Allow-Origin: *",
           })
         if (res.status == 200) {
           let data = res.data.data;
+          console.log(res);
           let dayCounter = {
             Monday: 0,
             Tuesday: 0,
@@ -155,8 +166,11 @@ export default {
           console.log(data.length);
           for (let i = 0; i < data.length; i++) {
             let day = moment(data[i].start).format("dddd");
+            dayCounter[day] += 1;
             weekWork[day] += (new Date(data[i].end) - new Date(data[i].start));
-            
+          }
+          for (let i = 0; i < 7; i++) {
+            weekWork[Object.keys(weekWork)[i]] /= dayCounter[Object.keys(dayCounter)[i]];
           }
           return weekWork;
         }
@@ -164,7 +178,17 @@ export default {
         console.log(err);
       }
     },
+    changeData() {
+      this.getWorkingTimeWeek();
+      this.getWorkingTimeWeek().then((weekWork) => {
+        this.lineData.datasets[0].data = [weekWork.Monday, weekWork.Tuesday, weekWork.Wednesday, weekWork.Thursday, weekWork.Friday, weekWork.Saturday, weekWork.Sunday];
+      })
+      this.getWorkingTimeEver().then((weekWork) => {
+        console.log(weekWork);
+        this.barsData.datasets[0].data = [weekWork.Monday, weekWork.Tuesday, weekWork.Wednesday, weekWork.Thursday, weekWork.Friday, weekWork.Saturday, weekWork.Sunday];
+      })
     }
+  }
 };
 </script>
 
