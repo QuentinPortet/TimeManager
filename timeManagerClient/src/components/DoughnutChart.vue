de5200ec77320ec28ac548cc9b9756b9d5fb833f
Arthur <arthur.rochette@gmail.com>
2022-11-03 11:07:15 +0100

    [~] add untracked files

---
+++ timeManagerClient/src/components/DoughnutChart.vue
@@ -0,0 +1,93 @@
+<script>
+import { Doughnut } from "vue-chartjs";
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  ArcElement,
+  CategoryScale,
+  LinearScale,
+} from "chart.js";
+
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  CategoryScale,
+  LinearScale,
+  ArcElement
+);
+
+export default {
+  name: "DoughnutChart",
+  components: { Doughnut },
+  props: {
+    chartId: {
+      type: String,
+      default: "doughnut-chart",
+    },
+    datasetIdKey: {
+      type: String,
+      default: "label",
+    },
+    width: {
+      type: Number,
+      default: 400,
+    },
+    height: {
+      type: Number,
+      default: 400,
+    },
+    cssClasses: {
+      default: "",
+      type: String,
+    },
+    styles: {
+      type: Object,
+      default: () => {},
+    },
+    plugins: {
+      type: Object,
+      default: () => {},
+    },
+    chartData: {
+      default: {
+        labels: ["Red", "Blue", "Yellow"],
+        datasets: [
+          {
+            label: "My First Dataset",
+            data: [300, 50, 100],
+            backgroundColor: [
+              "rgb(255, 99, 132)",
+              "rgb(54, 162, 235)",
+              "rgb(255, 205, 86)",
+            ],
+          },
+        ],
+      },
+    },
+  },
+  data() {
+    return {
+      chartOptions: {
+        responsive: true,
+      },
+    };
+  },
+};
+</script>
+
+<template>
+  <Doughnut
+    :chart-options="chartOptions"
+    :chart-data="chartData"
+    :chart-id="chartId"
+    :dataset-id-key="datasetIdKey"
+    :plugins="plugins"
+    :css-classes="cssClasses"
+    :styles="styles"
+    :width="width"
+    :height="height"
+  />
+</template>
